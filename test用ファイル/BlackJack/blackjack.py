c797519ed0e4d0b0987023fe1aa46f32c498f61a
kamaboko0411 <191832196+kamaboko0411@users.noreply.github.com.>
2024-12-19 15:25:44 +0900

    ブラックジャック更新

--- test用ファイル/BlackJack/blackjack.py
+++ test用ファイル/BlackJack/blackjack.py
@@ -94,25 +94,3 @@ class BlackJack:
 
     def get_user_betcoin(self):
         return self.__user_betcoin
-
-
-class Hands:
-    def __init__(self):
-        pass
-    def clear_hands(self):
-        pass
-    def get_hands_list(self):
-        return []
-    def add_hands(self, suit, number):
-        pass
-    def cal_hands_strength(self):
-        pass
-    def get_hands_strength(self):
-        return 0
-class Cards:
-    def __init__(self):
-        pass
-    def draw_card(self):
-        return ('spade', 1)
-    def reset_card(self):
-        pass
